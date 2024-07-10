234011cd45d8941d5e7717c3a9771fa07d8bcffc
1F10230104 <s1f102301044@iniad.org>
2024-07-10 23:39:17 +0900

    コードを修正

--- todo/tests.py
+++ todo/tests.py
@@ -7,10 +7,11 @@ from todo.models import Task
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
+        
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
-        due = timezone.make_aware (datetime (2024, 6, 30, 23, 59, 59))
+        due = timezone.make_aware(datetime (2024, 6, 30, 23, 59, 59))
         task = Task(title='task1', due_at=due)
         task.save()
         
